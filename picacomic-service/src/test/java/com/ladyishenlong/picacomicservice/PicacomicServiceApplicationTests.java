9807d3fa4964b397cc3b4683e0971638767ec598
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2019-09-30 14:12:21 +0800

    save

---
+++ picacomic-service/src/test/java/com/ladyishenlong/picacomicservice/PicacomicServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ladyishenlong.picacomicservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PicacomicServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
