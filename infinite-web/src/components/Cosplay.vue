1e7c272b12f234cc32cb23f3b7775d3e645b7427
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2019-10-12 10:15:19 +0800

    vue

--- infinite-web/src/components/Cosplay.vue
+++ infinite-web/src/components/Cosplay.vue
@@ -1,27 +1,81 @@
 <template>
 
-  <div><button class="primary" @click="cosplay">请求测试</button></div>
+  <div>
+
+    <el-row :gutter="20">
+
+      <div v-for=" modelItem in cosplayModels">
+        <el-col :md="6">
+          <el-card class="box-card">
+
+            <div>
+              <el-image 
+                        :src="modelItem.cover" :fit="cover" lazy></el-image>
+            </div>
+
+
+            <!--            <div>{{modelItem.setid}}</div>-->
+            <div>咸鱼不行</div>
+          </el-card>
+        </el-col>
+      </div>
+
+    </el-row>
+
+    <ElButton class="primary" @click="cosplay">请求测试</ElButton>
+  </div>
 
 </template>
 
 <script>
   export default {
     name: "Cosplay",
-    data(){
-      isoyuModel:{
-        
+    data() {
+      return {
+        page: 10,
+        isoyuModel: {
+          code: 0,
+          message: '',
+          data: null
+        },
+        cosplayModels: [],
+        cosplayModel: {
+          postid: '',
+          desc: '',
+          pvnum: '',
+          createdate: '',
+          scover: '',
+          setname: '',
+          cover: '',
+          pics: [],
+          clientcover1: '',
+          replynum: '',
+          topicname: '',
+          setid: '',
+          seturl: '',
+          datetime: '',
+          clientcover: '',
+          imgsum: '',
+          tcover: ''
+        }
       }
     },
     methods: {
-      cosplay(){
-        this.axios.get('/isoyu/cosplay/10')
-          .then((result)=>{
-            console.log(JSON.stringify(result.data))
+      cosplay() {
+        this.axios.get('/isoyu/cosplay/' + this.page)
+          .then((result) => {
+            this.isoyuModel = result.data;
+            if (this.isoyuModel.code === 0) {
+              //for循环加入数组
+              this.isoyuModel.data.forEach(model => {
+                this.cosplayModel = model;
+                this.cosplayModels.push(this.cosplayModel);
+              })
+            } else console.log(this.isoyuModel.message)
           })
-          .catch((error)=>{
+          .catch((error) => {
             console.log(error)
           })
-
       }
     }
   }
--- infinite-web/src/router/index.js
+++ infinite-web/src/router/index.js
@@ -2,14 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Cosplay from '@/components/Cosplay'
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.use(Router);
-Vue.use(iView);
+Vue.use(ElementUI);
 Vue.use(VueAxios,axios);
 
 axios.defaults.baseURL="http://localhost:7000";
