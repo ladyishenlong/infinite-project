ce996ff041186d5082ed7cfb0f5894af603deedd
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2019-10-28 13:58:24 +0800

    spring 用token作登录验证

--- test-service/src/main/java/com/ladyishenlong/testservice/controller/HelloController.java
+++ test-service/src/main/java/com/ladyishenlong/testservice/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.ladyishenlong.testservice.controller;
 
 import com.ladyishenlong.responseutils.ResponseUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,5 +16,6 @@ public class HelloController {
     public ResponseUtils hello(){
         return ResponseUtils.success("hello world");
     }
+
 
 }
