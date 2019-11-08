f09d0654d02aa76ea6234e4c44d207c80af319f7
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2019-11-08 15:04:06 +0800

    require.js引入css样式的插件

--- knockout-web/src/main/resources/static/js/ko.js
+++ knockout-web/src/main/resources/static/js/ko.js
@@ -1,46 +1,43 @@
-requirejs(['/js/resource.js'], function () {//先导入配置文件
-    requirejs(['jquery', 'knockout'], function ($, ko) {
 
-        var viewModel = function () {
-            var self = this;
-            self.isCheck = ko.observable(false);
+require(['/js/resource.js'], function () {//先导入配置文件
+    require(['jquery', 'knockout', 'bootstrap','koStyle'],
+        function ($, ko, bootstrap,koStyle) {
+            var viewModel = function () {
+                var self = this;
+                self.isCheck = ko.observable(false);
 
-            //表格的添加以及删除
-            self.table = ko.observableArray([]);
-            self.tableAdd = function () {
-                self.table.push({firstItem: "第一项", lastItem: "第二项"})
+                //表格的添加以及删除
+                self.table = ko.observableArray([]);
+                self.tableAdd = function () {
+                    self.table.push({firstItem: "第一项", lastItem: "第二项"})
+                };
+                self.removeTableOne = function () {
+                    self.table.remove(this);
+                };
+
+                self.showColor = ko.observable(true);
+
+
+                //event绑定
+                self.showorhideText = ko.observable(false);
+                self.showText = function () {
+                    self.showorhideText(true);
+                };
+                self.hideText = function () {
+                    self.showorhideText(false)
+                };
+
+                //value绑定
+                self.username = ko.observable();
+                self.password = ko.observable();
+
+                self.user = ko.computed(function () {
+                    return self.username() + " " + self.password();
+                }, this)
+
             };
-            self.removeTableOne = function () {
-                self.table.remove(this);
-            };
-
-            self.showColor = ko.observable(true);
-
-
-            //event绑定
-            self.showorhideText = ko.observable(false);
-            self.showText = function () {
-                self.showorhideText(true);
-            };
-            self.hideText = function () {
-                self.showorhideText(false)
-            };
-
-            //value绑定
-            self.username = ko.observable();
-            self.password = ko.observable();
-
-            self.user = ko.computed(function () {
-                return self.username() + " " + self.password();
-            }, this)
-
-        };
-
-
-        ko.applyBindings(new viewModel());
-
-
-    });
+            ko.applyBindings(new viewModel());
+        });
 });
 
 
--- knockout-web/src/main/resources/static/js/resource.js
+++ knockout-web/src/main/resources/static/js/resource.js
@@ -1,11 +1,25 @@
-requirejs.config({
+require.config({
+    map: {
+        '*': {
+            css: "https://cdn.bootcss.com/require-css/0.1.10/css.js" //集成require-css插件
+        }
+    },
     paths: {
         //在线地址才需要把原本最后的.js去除才行
         jquery: "https://code.jquery.com/jquery-1.9.1.min",
         knockout: "https://knockoutjs.com/downloads/knockout-3.5.0",
         bootstrap: "https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min",
+
+        koStyle:"/js/base",
     },
     shim: { //定义css
+        bootstrap: {
+            deps: ['css!https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css']
+        },
+
+        koStyle:{
+            deps:['jquery','css!/css/koStyle.css']
+        }
 
     }
 });
